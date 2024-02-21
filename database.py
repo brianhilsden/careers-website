9585b09be13cbda64bdf206fbe66af4c219c76bc
brianhilsden <146356642+brianhilsden@users.noreply.github.com>
2024-02-21 19:29:08 +0300

    added requirements

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-
+import os
 from sqlalchemy import create_engine,text
-
-
-engine = create_engine("mysql+pymysql://root:-BgbEEE646GGeEF-Fg316DAG2ff55Gce@roundhouse.proxy.rlwy.net:51741/railway?charset=utf8mb4")
+from dotenv import load_dotenv
+load_dotenv()
+engine = create_engine(os.getenv("db_connection"))
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
